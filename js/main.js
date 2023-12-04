0918a59d80b25cadf3f2c89186cfcfbefcd2420d
merankori <merankori228@gmail.com>
2023-12-04 23:48:38 +0300

    add hiding hud functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import { getTimeByZone } from "./utils/getTimeByZone.js";
 
-const hideMarqueeTime = 30000;
+const hideHudTime = 300000;
 
+const container = document.querySelector(".container");
 const marqueeWrapper = document.querySelector(".marquee-wrapper");
 const marquee = document.querySelector(".marquee");
 const messageAuthor = document.querySelector(".message__author");
@@ -9,8 +10,12 @@ const timeEl = document.querySelector(".info__time");
 
 let marqueeIndex = 0;
 
+const hideMarquee = () => {
+  marqueeWrapper.style.opacity = 0;
+  messageAuthor.style.opacity = 0;
+};
+
 const allowMarquee = (messages) => {
-  console.log(marqueeIndex);
   marqueeWrapper.style.opacity = 1;
   messageAuthor.style.opacity = 1;
   marquee.innerText = messages[marqueeIndex++];
@@ -22,8 +27,6 @@ const allowMarquee = (messages) => {
   const wrapperWidth = marqueeWrapper.offsetWidth;
   const marqueeWidth = marquee.offsetWidth;
   const animationTime = (marqueeWidth / 40) * 1000;
-
-  console.log(animationTime);
 
   const animation = marquee.animate(
     [
@@ -39,11 +42,12 @@ const allowMarquee = (messages) => {
     }
   );
 
-  animation.addEventListener("finish", () => {
-    marqueeWrapper.style.opacity = 0;
-    messageAuthor.style.opacity = 0;
-    setTimeout(() => allowMarquee(messages), hideMarqueeTime);
-  });
+  return new Promise((res) =>
+    animation.addEventListener("finish", () => {
+      hideMarquee();
+      res();
+    })
+  );
 };
 
 const allowCurrentInfo = () => {
@@ -55,13 +59,29 @@ const allowCurrentInfo = () => {
   );
 };
 
+const showHud = async (messages) => {
+  container.style.opacity = 1;
+
+  allowCurrentInfo();
+  await allowMarquee(messages);
+
+  container.style.opacity = 0;
+
+  return Promise.resolve();
+};
+
+const hudLoop = async (messages) => {
+  await showHud(messages);
+
+  setTimeout(() => hudLoop(messages), hideHudTime);
+};
+
 const start = async () => {
   const res = await fetch("data.json");
   const data = await res.json();
 
   if (data) {
-    allowMarquee(data.messages);
-    allowCurrentInfo();
+    setTimeout(() => hudLoop(data.messages), hideHudTime);
   }
 };
 
